355b188db5a365befc970b4ed580346b21625b0c
Max <max.gaspard@outlook.fr>
2022-11-06 16:33:19 +0100

    add bootswatch theme, category creation view and category create method

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
 ﻿using BulkyBookWeb.Data;
+using BulkyBookWeb.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BulkyBookWeb.Controllers
@@ -14,8 +15,28 @@ namespace BulkyBookWeb.Controllers
 
         public IActionResult Index()
         {
-            var objCategoryList = _db.Categories.ToList();
+            IEnumerable<Category> objCategoryList = _db.Categories;
+            return View(objCategoryList);
+        }
+
+        //GET
+        public IActionResult Create()
+        {
             return View();
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category formData)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(formData);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(formData);
         }
     }
 }
