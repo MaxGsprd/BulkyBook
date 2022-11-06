05bd8f15ca4b12f5764c85349c54c98805778d64
Max <max.gaspard@outlook.fr>
2022-11-06 10:58:36 +0100

    add CategoryController and Category view

--- BulkyBookWeb/Data/ApplicationDbContext.cs
+++ BulkyBookWeb/Data/ApplicationDbContext.cs
@@ -3,7 +3,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace BulkyBookWeb.Data
 {
-    public class ApplicationDbContext :DbContext
+    public class ApplicationDbContext : DbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
